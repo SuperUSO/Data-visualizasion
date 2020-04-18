9c41c22f03949b76e9a48faf2d38864bcd410d1e
p-hantom <3137359368@qq.com>
2020-04-18 20:37:54 +0800

    add vue project

---
+++ vue-data-visualization/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p>App.vue</p>
+    <VChart />
+    <BarChart />
+    <PieAndLineChart />
+    <LineChart />
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import VChart from '@/components/V-Chart'
+import LineChart from '@/components/LineChart'
+import BarChart from '@/components/BarChart'
+import PieAndLineChart from '@/components/PieAndLineChart'
+export default {
+  name: 'App',
+  components: {
+    VChart,
+    LineChart,
+    BarChart,
+    PieAndLineChart
+    // WordCloud2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
