951ead6478614a58d174c5dcc9c62ba5215ab531
long <1600691512@qq.com>
2020-04-20 22:04:42 +0800

    vue-cli use ui

---
+++ test/src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+	<h2 class="red">test....{{msg}}</h2>
+</template>
+
+<script>
+export default{
+	name: 'HelloWorld',
+	props:{
+		msg: {
+			type: String,
+			default: "test msg"
+		}
+	},
+	methods:{
+		
+		
+	},
+	data(){
+		return {
+			
+		}
+	}
+}	
+</script>
+
+<style>
+.red{
+	color: red;
+}	
+</style>
